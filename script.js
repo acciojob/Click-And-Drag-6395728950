e427ad042ca408f5f9cac52153939aeeb2e408b7
6395728950 <161918761+6395728950@users.noreply.github.com>
2025-05-02 16:45:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 // Your code here.
+const cubes = document.querySelectorAll('.item');
+cubes.forEach((cube,index)=>{
+	cube.addEventListener("mousedown",(e)=>{
+		 // set the draggable attribute
+		cube.setAttribute('draggable',true);
+		// save the inital position
+		const offsetX = e.clientX-cube.getBoundingClientReact().left;
+		const offsetY = e.clientY-cube.getBoundingClientReact().top;
+		const mouseMoveHandler = (e) =>{
+			cube.style.position = 'absolute';
+			cube.style.left = (e.clientX-offsetX)+'px';
+			cube.style.top = (e.clientY-offsetY)+'px';
+		};
+		const mouseUpHandler = ()=>{
+			// Remove the event Listener when mouse is released
+			document.removeEventListener("mousemove",mouseMoveHandler);
+		document.removeEventListener("mouseup",mouseUpHandler);
+		};
+		
+document.addEventListener("mousemove",mouseMoveHandler);
+		document.addEventListener("mouseup",mouseUpHandler);
+	});
+});
+
+
+	
+})
